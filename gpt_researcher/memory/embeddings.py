8e3aaf1c6794a6ff80f180e59d3683f70efc39b1
Taher Ali badnawarwala <tahercoolguy@gmail.com>
2024-04-05 16:22:00 +0530

    embedding also changed

--- gpt_researcher/memory/embeddings.py
+++ gpt_researcher/memory/embeddings.py
@@ -1,7 +1,12 @@
 from langchain_community.vectorstores import FAISS
 import os
 
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
+import os
+
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = "AIzaSyBBKHWk5C8Ar7A1EEWuhfX2jYqQAYZbPj0"
 class Memory:
     def __init__(self, embedding_provider, **kwargs):
 
@@ -19,7 +24,8 @@ class Memory:
             case "huggingface":
                 from langchain.embeddings import HuggingFaceEmbeddings
                 _embeddings = HuggingFaceEmbeddings()
-
+            case "google":
+                _embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001") #OpenAIEmbeddings()
             case _:
                 raise Exception("Embedding provider not found.")
 
